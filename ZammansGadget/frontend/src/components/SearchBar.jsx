9885cf28cc8cc01904f52dfd35fda92de96c5713
mdshoaib77 <email4966@gmail.com>
2025-07-28 10:21:21 +0600

    Alhamdulliah seachbar update

--- ZammansGadget/frontend/src/components/SearchBar.jsx
+++ ZammansGadget/frontend/src/components/SearchBar.jsx
@@ -1,32 +1,75 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+// import React, { useContext, useEffect, useState } from 'react'
+// import { ShopContext } from '../context/ShopContext'
+// import { assets } from '../assets/assets';
+// import { useLocation } from 'react-router-dom';
+
+// const SearchBar = () => {
+
+//     const { search, setSearch, showSearch, setShowSearch} = useContext(ShopContext);
+//     const [visible,setVisible] = useState(false)
+//     const location = useLocation();
+
+//     useEffect(()=>{
+//         if (location.pathname.includes('collection')) {
+//             setVisible(true);
+//         }
+//         else {
+//             setVisible(false)
+//         }
+//     },[location])
+    
+//   return showSearch && visible ? (
+//     <div className='mt-16 text-center border-t border-b bg-gray-50'>
+//       <div className='inline-flex items-center justify-center w-3/4 px-5 py-2 mx-3 my-5 border border-gray-400 rounded-full sm:w-1/2'>
+//         <input value={search} onChange={(e)=>setSearch(e.target.value)} className='flex-1 text-sm outline-none bg-inherit' type="text" placeholder='Search'/>
+//         <img className='w-4' src={assets.search_icon} alt="" />
+//       </div>
+//       <img onClick={()=>setShowSearch(false)} className='inline w-3 cursor-pointer' src={assets.cross_icon} alt="" />
+//     </div>
+//   ) : null
+// }
+
+// export default SearchBar
+
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
 import { useLocation } from 'react-router-dom';
 
 const SearchBar = () => {
+  const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
+  const [visible, setVisible] = useState(false);
+  const location = useLocation();
 
-    const { search, setSearch, showSearch, setShowSearch} = useContext(ShopContext);
-    const [visible,setVisible] = useState(false)
-    const location = useLocation();
+  useEffect(() => {
+    // Show search bar only on paths containing 'collection'
+    if (location.pathname.includes('collection')) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, [location]);
 
-    useEffect(()=>{
-        if (location.pathname.includes('collection')) {
-            setVisible(true);
-        }
-        else {
-            setVisible(false)
-        }
-    },[location])
-    
   return showSearch && visible ? (
-    <div className='mt-16 text-center border-t border-b bg-gray-50'>
-      <div className='inline-flex items-center justify-center w-3/4 px-5 py-2 mx-3 my-5 border border-gray-400 rounded-full sm:w-1/2'>
-        <input value={search} onChange={(e)=>setSearch(e.target.value)} className='flex-1 text-sm outline-none bg-inherit' type="text" placeholder='Search'/>
-        <img className='w-4' src={assets.search_icon} alt="" />
+    <div className="mt-16 text-center border-t border-b bg-gray-50">
+      <div className="inline-flex items-center justify-center w-3/4 px-5 py-2 mx-3 my-5 border border-gray-400 rounded-full sm:w-1/2">
+        <input
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="flex-1 text-sm outline-none bg-inherit"
+        />
+        <img className="w-4" src={assets.search_icon} alt="search icon" />
       </div>
-      <img onClick={()=>setShowSearch(false)} className='inline w-3 cursor-pointer' src={assets.cross_icon} alt="" />
+      <img
+        onClick={() => setShowSearch(false)}
+        className="inline w-3 cursor-pointer"
+        src={assets.cross_icon}
+        alt="close icon"
+      />
     </div>
-  ) : null
-}
+  ) : null;
+};
 
-export default SearchBar
+export default SearchBar;
--- ZammansGadget/frontend/src/context/ShopContext.jsx
+++ ZammansGadget/frontend/src/context/ShopContext.jsx
@@ -1,7 +1,179 @@
+// import { createContext, useEffect, useState } from "react";
+// import { toast } from "react-toastify";
+// import { useNavigate } from "react-router-dom";
+// import axios from 'axios'
+
+// export const ShopContext = createContext();
+
+// const ShopContextProvider = (props) => {
+
+//     const currency = '৳ BDT ';
+//     const delivery_fee = 100;
+//     const backendUrl = import.meta.env.VITE_BACKEND_URL
+//     const [search, setSearch] = useState('');
+//     const [showSearch, setShowSearch] = useState(false);
+//     const [cartItems, setCartItems] = useState({});
+//     const [products, setProducts] = useState([]);
+//     const [token, setToken] = useState('')
+//     const navigate = useNavigate();
+
+
+//     const addToCart = async (itemId, size) => {
+
+//         if (!size) {
+//             toast.error('Select Product Size');
+//             return;
+//         }
+
+//         let cartData = structuredClone(cartItems);
+
+//         if (cartData[itemId]) {
+//             if (cartData[itemId][size]) {
+//                 cartData[itemId][size] += 1;
+//             }
+//             else {
+//                 cartData[itemId][size] = 1;
+//             }
+//         }
+//         else {
+//             cartData[itemId] = {};
+//             cartData[itemId][size] = 1;
+//         }
+//         setCartItems(cartData);
+
+//         if (token) {
+//             try {
+
+//                 await axios.post(backendUrl + '/api/cart/add', { itemId, size }, { headers: { token } })
+
+//             } catch (error) {
+//                 console.log(error)
+//                 toast.error(error.message)
+//             }
+//         }
+
+//     }
+
+//     const getCartCount = () => {
+//         let totalCount = 0;
+//         for (const items in cartItems) {
+//             for (const item in cartItems[items]) {
+//                 try {
+//                     if (cartItems[items][item] > 0) {
+//                         totalCount += cartItems[items][item];
+//                     }
+//                 } catch (error) {
+
+//                 }
+//             }
+//         }
+//         return totalCount;
+//     }
+
+//     const updateQuantity = async (itemId, size, quantity) => {
+
+//         let cartData = structuredClone(cartItems);
+
+//         cartData[itemId][size] = quantity;
+
+//         setCartItems(cartData)
+
+//         if (token) {
+//             try {
+
+//                 await axios.post(backendUrl + '/api/cart/update', { itemId, size, quantity }, { headers: { token } })
+
+//             } catch (error) {
+//                 console.log(error)
+//                 toast.error(error.message)
+//             }
+//         }
+
+//     }
+
+//     const getCartAmount = () => {
+//         let totalAmount = 0;
+//         for (const items in cartItems) {
+//             let itemInfo = products.find((product) => product._id === items);
+//             for (const item in cartItems[items]) {
+//                 try {
+//                     if (cartItems[items][item] > 0) {
+//                         totalAmount += itemInfo.price * cartItems[items][item];
+//                     }
+//                 } catch (error) {
+
+//                 }
+//             }
+//         }
+//         return totalAmount;
+//     }
+
+//     const getProductsData = async () => {
+//         try {
+
+//             const response = await axios.get(backendUrl + '/api/product/list')
+//             if (response.data.success) {
+//                 setProducts(response.data.products.reverse())
+//             } else {
+//                 toast.error(response.data.message)
+//             }
+
+//         } catch (error) {
+//             console.log(error)
+//             toast.error(error.message)
+//         }
+//     }
+
+//     const getUserCart = async ( token ) => {
+//         try {
+            
+//             const response = await axios.post(backendUrl + '/api/cart/get',{},{headers:{token}})
+//             if (response.data.success) {
+//                 setCartItems(response.data.cartData)
+//             }
+//         } catch (error) {
+//             console.log(error)
+//             toast.error(error.message)
+//         }
+//     }
+
+//     useEffect(() => {
+//         getProductsData()
+//     }, [])
+
+//     useEffect(() => {
+//         if (!token && localStorage.getItem('token')) {
+//             setToken(localStorage.getItem('token'))
+//             getUserCart(localStorage.getItem('token'))
+//         }
+//         if (token) {
+//             getUserCart(token)
+//         }
+//     }, [token])
+
+//     const value = {
+//         products, currency, delivery_fee,
+//         search, setSearch, showSearch, setShowSearch,
+//         cartItems, addToCart,setCartItems,
+//         getCartCount, updateQuantity,
+//         getCartAmount, navigate, backendUrl,
+//         setToken, token
+//     }
+
+//     return (
+//         <ShopContext.Provider value={value}>
+//             {props.children}
+//         </ShopContext.Provider>
+//     )
+
+// }
+
+// export default ShopContextProvider;
+
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'
+import axios from 'axios';
 
 export const ShopContext = createContext();
 
@@ -9,33 +181,27 @@ const ShopContextProvider = (props) => {
 
     const currency = '৳ BDT ';
     const delivery_fee = 100;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
     const [products, setProducts] = useState([]);
-    const [token, setToken] = useState('')
+    const [token, setToken] = useState('');
     const navigate = useNavigate();
 
-
     const addToCart = async (itemId, size) => {
-
         if (!size) {
             toast.error('Select Product Size');
             return;
         }
-
         let cartData = structuredClone(cartItems);
-
         if (cartData[itemId]) {
             if (cartData[itemId][size]) {
                 cartData[itemId][size] += 1;
-            }
-            else {
+            } else {
                 cartData[itemId][size] = 1;
             }
-        }
-        else {
+        } else {
             cartData[itemId] = {};
             cartData[itemId][size] = 1;
         }
@@ -43,16 +209,13 @@ const ShopContextProvider = (props) => {
 
         if (token) {
             try {
-
-                await axios.post(backendUrl + '/api/cart/add', { itemId, size }, { headers: { token } })
-
+                await axios.post(backendUrl + '/api/cart/add', { itemId, size }, { headers: { token } });
             } catch (error) {
-                console.log(error)
-                toast.error(error.message)
+                console.log(error);
+                toast.error(error.message);
             }
         }
-
-    }
+    };
 
     const getCartCount = () => {
         let totalCount = 0;
@@ -62,34 +225,26 @@ const ShopContextProvider = (props) => {
                     if (cartItems[items][item] > 0) {
                         totalCount += cartItems[items][item];
                     }
-                } catch (error) {
-
-                }
+                } catch (error) {}
             }
         }
         return totalCount;
-    }
+    };
 
     const updateQuantity = async (itemId, size, quantity) => {
-
         let cartData = structuredClone(cartItems);
-
         cartData[itemId][size] = quantity;
-
-        setCartItems(cartData)
+        setCartItems(cartData);
 
         if (token) {
             try {
-
-                await axios.post(backendUrl + '/api/cart/update', { itemId, size, quantity }, { headers: { token } })
-
+                await axios.post(backendUrl + '/api/cart/update', { itemId, size, quantity }, { headers: { token } });
             } catch (error) {
-                console.log(error)
-                toast.error(error.message)
+                console.log(error);
+                toast.error(error.message);
             }
         }
-
-    }
+    };
 
     const getCartAmount = () => {
         let totalAmount = 0;
@@ -100,72 +255,73 @@ const ShopContextProvider = (props) => {
                     if (cartItems[items][item] > 0) {
                         totalAmount += itemInfo.price * cartItems[items][item];
                     }
-                } catch (error) {
-
-                }
+                } catch (error) {}
             }
         }
         return totalAmount;
-    }
+    };
 
     const getProductsData = async () => {
         try {
-
-            const response = await axios.get(backendUrl + '/api/product/list')
+            const response = await axios.get(backendUrl + '/api/product/list');
             if (response.data.success) {
-                setProducts(response.data.products.reverse())
+                setProducts(response.data.products.reverse());
             } else {
-                toast.error(response.data.message)
+                toast.error(response.data.message);
             }
-
         } catch (error) {
-            console.log(error)
-            toast.error(error.message)
+            console.log(error);
+            toast.error(error.message);
         }
-    }
+    };
 
-    const getUserCart = async ( token ) => {
+    const getUserCart = async (token) => {
         try {
-            
-            const response = await axios.post(backendUrl + '/api/cart/get',{},{headers:{token}})
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, { headers: { token } });
             if (response.data.success) {
-                setCartItems(response.data.cartData)
+                setCartItems(response.data.cartData);
             }
         } catch (error) {
-            console.log(error)
-            toast.error(error.message)
+            console.log(error);
+            toast.error(error.message);
         }
-    }
+    };
 
     useEffect(() => {
-        getProductsData()
-    }, [])
+        getProductsData();
+    }, []);
 
     useEffect(() => {
         if (!token && localStorage.getItem('token')) {
-            setToken(localStorage.getItem('token'))
-            getUserCart(localStorage.getItem('token'))
+            setToken(localStorage.getItem('token'));
+            getUserCart(localStorage.getItem('token'));
         }
         if (token) {
-            getUserCart(token)
+            getUserCart(token);
         }
-    }, [token])
+    }, [token]);
 
     const value = {
-        products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch,
-        cartItems, addToCart,setCartItems,
-        getCartCount, updateQuantity,
-        getCartAmount, navigate, backendUrl,
-        setToken, token
-    }
+        products,
+        currency,
+        delivery_fee,
+        search,
+        setSearch,
+        showSearch,
+        setShowSearch,
+        cartItems,
+        addToCart,
+        setCartItems,
+        getCartCount,
+        updateQuantity,
+        getCartAmount,
+        navigate,
+        backendUrl,
+        setToken,
+        token,
+    };
 
-    return (
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-
-}
+    return <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>;
+};
 
 export default ShopContextProvider;
